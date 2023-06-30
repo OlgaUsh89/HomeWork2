c834526fe93de652003f2b14a9ac7cb53ba95b76
Olya test <ushakovaolga1989@gmail.com>
2023-06-30 13:26:52 +0300

    Calculation bonus added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        int account = 300;
+        int replenishment = 1500;
+
+
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int total = account + replenishment + bonus;
+        System.out.println(" Итоговый счет " + total);
+        System.out.println(" Итоговый бонус " + bonus);
+
+    }
+
+
 }
